9402ab4fe6436d49d6737bc26ff9c9d5faf9cf65
take000 <40623481+take000@users.noreply.github.com>
2020-08-27 10:39:16 +0900

    エラーページに飛ぶように変更

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import { listenAndServe } from "https://deno.land/std/http/server.ts";
 import { acceptWebSocket, acceptable } from "https://deno.land/std/ws/mod.ts";
-import { Server } from "https://code4sabae.github.io/js/Server.js";
+import { createApp } from "https://servestjs.org/@v1.1.2/mod.ts";
+import { CONTENT_TYPE } from "https://code4sabae.github.io/js/CONTENT_TYPE.js";
 import chat from "./chat.js";
 
 listenAndServe({ port: 8883 }, async (req) => {
@@ -15,6 +16,44 @@ listenAndServe({ port: 8883 }, async (req) => {
     }
   }
 });
+
+class Server {
+  constructor(port) {
+    const app = createApp();
+    app.handle(/\/*/, async (req) => {
+      if (req.path.startsWith("/ws/")) {
+        return;
+      }
+      try {
+        const fn = req.path === "/" || req.path.indexOf("..") >= 0
+          ? "/index.html"
+          : req.path;
+        const n = fn.lastIndexOf(".");
+        const ext = n < 0 ? "html" : fn.substring(n + 1);
+        const data = Deno.readFileSync("static" + fn);
+        const ctype = CONTENT_TYPE[ext] || "text/plain";
+        await req.respond({
+          status: 200,
+          headers: new Headers({ "Content-Type": ctype }),
+          body: data,
+        });
+      } catch (e) {
+        const errorPage = Deno.readFileSync("./static/error.html");
+        req.respond({
+          status: 404,
+          headers: new Headers({ "Content-Type": "text/html" }),
+          body: errorPage,
+        });
+        if (req.path !== "/favicon.ico") {
+          console.log("err", req.path, e.stack);
+        }
+      }
+    });
+
+    app.listen({ port });
+    console.log(`http://localhost:${port}/`);
+  }
+}
 
 new Server(8884);
 console.log("↑ここにGo!");
